7dce91a33c87c3f82c009f76e209346e99a9f779
AavinH <82467140+AavinH@users.noreply.github.com>
2021-05-13 13:54:00 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Xr5!$Ne;@iU(KK{*5hwL":
+            case "fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 2 . . . . . . 
+. . . . 2 2 4 4 2 2 2 . . . . . 
+. . . 2 2 4 4 4 4 2 2 2 . . . . 
+. . 2 2 4 4 4 2 2 4 4 2 2 . . . 
+. . 2 4 2 2 4 4 4 2 2 4 2 2 . . 
+. 2 4 2 2 2 2 4 4 4 2 4 4 2 . . 
+. 2 4 2 2 4 2 2 4 2 4 4 2 2 . . 
+2 4 2 2 2 2 4 4 4 2 2 4 2 . . . 
+2 4 2 2 4 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 4 4 4 4 4 4 2 . . . . 
+. 2 4 4 4 2 2 4 4 4 2 . . . . . 
+. 2 2 4 4 4 4 4 2 2 . . . . . . 
+. . . 2 4 2 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
